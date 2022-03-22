7e92dfd88ddb88396429f6cb4195f4f42b87ab48
vansh4 <80308057+vansh4@users.noreply.github.com>
2022-03-22 13:39:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+rightwristX=0;
+leftwristX=0;
+function setup(){
+    video= createCapture(VIDEO);
+    video.size(550,500);
+     canvas=createCanvas(550,550);
+     canvas.position(560,150);
+     poseNet=ml5.poseNet(video,modelLoaded);
+     poseNet.on('pose',gotPoses);
+}
+ function modelLoaded(){
+     console.log('PoseNet is initialzed');
+ }
+
+ function gotPoses(results) 
+ { 
+     if(results.length > 0) 
+     { console.log(results); 
+        leftWristX = results[0].pose.leftWrist.x; 
+        rightWristX = results[0].pose.rightWrist.x; 
+        difference = floor(leftWristX - rightWristX); 
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference); 
+    } 
+} 
+        
+        function draw() 
+        { background('#6C91C2'); 
+        document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px"; 
+        textSize(difference); fill('#FFE787'); 
+        text('Vansh',50, 400); }
